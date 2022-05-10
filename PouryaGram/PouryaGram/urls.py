3b449ff1df9c7b2e1624728c2ccd670db6509638
pouryajanparvar <janparvarpourya@gmail.com>
2022-05-10 17:37:04 +0430

    create register page

--- PouryaGram/PouryaGram/urls.py
+++ PouryaGram/PouryaGram/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls', namespace='home'))
+    path('', include('home.urls', namespace='home')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
--- PouryaGram/accounts/views.py
+++ PouryaGram/accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.views import View
 # Create your views here.
+
+
+class UserRegisterView(View):
+    def get(self, request):
+        return render(request, 'accounts/register_page.html')
+
+    def post(self, request):
+        pass
+
+
