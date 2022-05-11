41b5ded7c555ad7c05787e3615a31d3cc6db3b3a
pouryajanparvar <janparvarpourya@gmail.com>
2022-05-11 12:11:37 +0430

    customizing forms with widget

--- PouryaGram/accounts/forms.py
+++ PouryaGram/accounts/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 class UserRegistrationsForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'username'}))
-    email = forms.EmailField()
-    password = forms.CharField()
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
